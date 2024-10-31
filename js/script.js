8dd5351fd23cfc63cf5ec08a584da8d52a33f700
Alhambra Ferdinando <105513029+NanoKeboku@users.noreply.github.com>
2024-10-31 21:17:30 +0700

    devisi, gform
    update by nando

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const myModal = document.getElementById('psdm')
+const myInput = document.querySelector('.modal-jobdesk')
+
+myModal.addEventListener('click', () => {
+  myModal.classList.add('active')
+})
